6a15ba9192c4698131292881ccac00689a84ae03
Winton Welsh <mail@wintoni.us>
2016-01-26 01:01:33 -0800

    Parse JSON options

--- src/basher/ext/args.js
+++ src/basher/ext/args.js
@@ -6,6 +6,18 @@ export default Class =>
       let argv = process.argv.slice(2)
       let options = minimist(argv)
 
+      for (let key in options) {
+        let value = options[key]
+
+        if (typeof value == "string") {
+          let first = value.charAt(0)
+
+          if ([ "{", "[" ].indexOf(first) > -1) {
+            options[key] = JSON.parse(value)
+          }
+        }
+      }
+
       super(...args, { options })
     }
   }
