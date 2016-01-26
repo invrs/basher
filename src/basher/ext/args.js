42534ecf31a3c97ffbdb5e5749b4a69338939339
Winton Welsh <mail@wintoni.us>
2016-01-25 16:27:29 -0800

    Pass all constructor args through on extensions

--- src/basher/ext/args.js
+++ src/basher/ext/args.js
@@ -2,9 +2,10 @@ import minimist from "minimist"
 
 export default Class =>
   class extends Class {
-    constructor(state) {
-      let argv = state.argv || process.argv.slice(2)
-      state.options = minimist(argv)
-      super(state)
+    constructor(...args) {
+      let argv = process.argv.slice(2)
+      let options = minimist(argv)
+
+      super(...args, { options })
     }
   }
--- src/basher/ext/cmd.js
+++ src/basher/ext/cmd.js
@@ -2,11 +2,11 @@ import factory from "../include/cmd"
 
 export default (Class) =>
   class extends Class {
-    constructor(state) {
+    constructor(...args) {
       let instance = factory(null)
       let cmd = (str) =>
         instance.cmd.bind(instance)({ add: str })
 
-      super({ cmd })
+      super(...args, { cmd })
     }
   }
