eee6c297157002589ed24761d78bdaf37a6f3aac
Winton Welsh <mail@wintoni.us>
2016-01-26 17:30:01 -0800

    Expose makeCmd and resetCmd

--- src/basher/ext/cmd.js
+++ src/basher/ext/cmd.js
@@ -1,16 +1,28 @@
 import factory from "../include/cmd"
 
+function makeCmd() {
+  let instance = factory(null)
+  return (...args) =>
+    instance.add.bind(instance)({ args })
+}
+
 export default (Class) =>
   class extends Class {
     constructor(...args) {
-      let instance = factory(null)
-      let cmd = (str) =>
-        instance.cmd.bind(instance)({ add: str })
+      let cmd = makeCmd()
 
       super(...args, { cmd })
 
       for (let [ name, fn ] of this.functions()) {
         this[name] = (...args) => fn(...args, { cmd })
       }
+    }
+
+    makeCmd() { return makeCmd() }
+
+    resetCmd() {
+      let cmd = makeCmd()
+      this.state({ cmd })
+      return cmd
     }
   }
